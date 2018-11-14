37b08af75f6206fa96fa2178b4ad91d393995a92
olivier <olivier@baosongrobot.com>
2018-11-14 19:40:35 +0800

    Demand : currency use 2 digits

--- src/main/webapp/app/shared/util/currency-custom.pipe.ts
+++ src/main/webapp/app/shared/util/currency-custom.pipe.ts
@@ -4,6 +4,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'currencyCustom' })
 export class CurrencyCustomPipe extends CurrencyPipe implements PipeTransform {
     transform(value: any, args?: any): any {
-        return super.transform(value, 'CNY', 'symbol-narrow', '1.0-3', 'en');
+        return super.transform(value, 'CNY', 'symbol-narrow', '1.2-3', 'en');
     }
 }
